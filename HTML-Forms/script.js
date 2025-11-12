0f519be2f9e831326be1c781a4278ecfc89f212c
Kit Bagasol <mjbolwar@gmail.com>
2025-11-13 03:35:42 +0800

    created a function that generates 6 digit hex code and use it as background color for the reset whenever the submit is clicked

--- HTML-Forms/script.js
+++ HTML-Forms/script.js
@@ -1,7 +1,21 @@
 const name = document.querySelector("#username");
 const container = document.querySelector(".the-greeting");
 const button = document.querySelector('.greet-me');
+const submit = document.querySelector('.submit');
+const reset = document.querySelector('.reset');
 
 button.addEventListener("click", () => {
     container.innerText = `Happy Birthday ${name.value}`;
 });
+
+submit.addEventListener("click", () => {
+    reset.style.backgroundColor = `${randomColor()}`;
+});
+
+function randomColor () {
+    const number = [0,1,2,3,4,5,6,7,8,9];
+    const sorted = number.sort((a, b) => Math.random() - 0.5);
+    const compressed = '#' + sorted.slice(0,6).join("");
+    return compressed;
+};
+
